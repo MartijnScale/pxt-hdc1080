46bcf561e374b515f5f6bc470b982fff070498c4
MartijnScale <36644394+MartijnScale@users.noreply.github.com>
2022-02-01 11:21:36 +0100

    Update test.ts
    Small test

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(function () {
+    basic.showNumber(HDC1080.getTemp())
+    basic.pause(2000)
+})
